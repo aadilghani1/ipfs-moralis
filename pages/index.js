898537cb9346fa2fb2d1d78cf6b8e6214d7f2313
Aadil Ghani <aadilghani1@gmail.com>
2021-12-04 16:43:17 +0530

    feat: ipfs storage with next and moralis

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,110 @@
-import Head from 'next/head'
-
+import Head from "next/head";
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import { useMoralis } from "react-moralis";
 export default function Home() {
+  const {
+    authenticate,
+    isAuthenticated,
+    user,
+    logout,
+    Moralis,
+    isInitialized,
+  } = useMoralis();
+  const [attachments, setAttachments] = useState([]);
+  const inputRef = useRef(null);
+  const [changeSwitch, setChangeSwitch] = useState(false);
+
+  const onChangeHandler = async (e) => {
+    const data = e.target.files[0];
+    const file = new Moralis.File(data.name, data);
+    await file.saveIPFS();
+    const jobApplication = new Moralis.Object("Testfiles");
+    jobApplication.set("name", "files");
+    jobApplication.set("fileData", file);
+    await jobApplication.save();
+    setChangeSwitch(!changeSwitch);
+  };
+
+  useEffect(() => {
+    if (isAuthenticated && isInitialized) {
+      const testAsync = async () => {
+        const query = new Moralis.Query("Testfiles");
+        query.equalTo("name", "files");
+        const op = await query.find();
+        const tempArr = [];
+        op.forEach((item) => tempArr.push(item.attributes.fileData.ipfs()));
+        setAttachments(tempArr);
+      };
+      testAsync();
+    }
+  }, [isInitialized, changeSwitch]);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+      {!user ? (
+        <>
+          <button
+            onClick={() => authenticate({ signingMessage: "Signing in" })}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+            login
+          </button>
+          <button
+            onClick={() =>
+              authenticate({
+                provider: "walletconnect",
+                signingMessage: "Signing in",
+              })
+            }
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+            login with wc
+          </button>
+        </>
+      ) : (
+        <div className="flex flex-col gap-3 items-center">
+          <input
+            type="file"
+            ref={inputRef}
+            onChange={onChangeHandler}
+            className="hidden"
+          />
+          <button
+            className="px-3 py-2 bg-red-600 text-white rounded-md hover:opacity-90 fixed top-5 right-5"
+            onClick={() => logout()}
           >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+            logout
+          </button>
+          <button
+            onClick={() => inputRef.current.click()}
+            className="px-3 py-2 bg-green-500 text-white hover:opacity-90 rounded-md"
           >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            Upload files
+          </button>
+
+          <HiddenScroll>
+            {attachments?.map((imageUrl, index) => (
+              <img
+                src={imageUrl}
+                alt="image"
+                key={index}
+                className="h-40 rounded-md"
+              />
+            ))}
+          </HiddenScroll>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      )}
     </div>
-  )
+  );
 }
+
+const HiddenScroll = styled.div`
+  display: flex;
+  flex-direction: row;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  column-gap: 1rem;
+  overflow-x: scroll;
+`;
